905e19eabe811d452dac75a5efac0afc6e23f5d5
Amit Mittal <amit.mittal1993@gmail.com>
2018-12-08 21:26:24 -0500

    Firefox logger added

---
+++ src/scripts/services/firefox/firefox-logger.ts
@@ -0,0 +1,21 @@
+import { injectable } from "inversify";
+import "reflect-metadata";
+import { ILogger } from "../../interfaces/logger.interface";
+
+@injectable()
+export class FirefoxLogger implements ILogger {
+  info(message: any): void {
+    this.write("INFO", message);
+  }
+
+  error(message: any): void {
+    this.write("ERROR", message);
+  }
+
+  private write(prefix: string, message: any): void {
+    const date = new Date();
+    const line: string = `${date.toTimeString().slice(0, 8)}:${date.getMilliseconds()}|${prefix}|${message}`;
+
+    console.log(line);
+  }
+}
